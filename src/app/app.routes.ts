1bcead44bef19725341211434ce3929cbb400dfc
emmanuelcabello <emmanuelcabello125@gmail.com>
2026-01-24 11:34:53 -0600

    Version final profesional

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HeroComponent } from './hero/hero-component';
+
+export const routes: Routes = [
+    { path: '', component: HeroComponent }
+];
